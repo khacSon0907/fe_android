4d0d5e69976bbd9dfe012a2b6892350b50f060cf
Bach Ly <ptbly99999@gmail.com>
2025-03-18 20:38:56 +0700

    re_update

--- app/src/main/java/com/example/myapplication/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/myapplication/fragments/ProfileFragment.java
@@ -1,42 +1,51 @@
 package com.example.myapplication.fragments;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.util.Log;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.myapplication.R;
-import com.example.myapplication.view.authentication.Register;
+import com.example.myapplication.model.ProfileOption;
+import com.example.myapplication.view.ProfileAdapter;
 
-import androidx.fragment.app.Fragment;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProfileFragment extends Fragment {
 
-    private Button btnItRgt;
+    private RecyclerView recyclerView;
+    private ProfileAdapter adapter;
+    private List<ProfileOption> options;
 
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        recyclerView = view.findViewById(R.id.recycler_profile_options);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        btnItRgt = view.findViewById(R.id.btnItRgt);
-        if (btnItRgt == null) {
-            Log.e("ProfileFragment", "btnItRgt is null");
-        } else {
-            btnItRgt.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (getActivity() == null) {
-                        Log.e("ProfileFragment", "Activity is null");
-                        return;
-                    }
-                    Intent intent = new Intent(getActivity(), Register.class);
-                    startActivity(intent);
-                }
-            });
-        }
+        options = new ArrayList<>();
+        options.add(new ProfileOption(R.drawable.ic_phone, "Hỗ Trợ","1900 545 403"));
+        options.add(new ProfileOption(R.drawable.ic_email, "Email","support@fado.vn"));
+        options.add(new ProfileOption(R.drawable.ic_order, "Đơn hàng","Kiểm tra đơn hàng"));
+        options.add(new ProfileOption(R.drawable.ic_language, "Ngôn ngữ","Đổi ngôn ngữ"));
+        options.add(new ProfileOption(R.drawable.ic_info, "Thông tin","Thông tin ứng dụng"));
+
+        adapter = new ProfileAdapter(options, new ProfileAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(ProfileOption option) {
+                // Xử lý khi item được nhấn
+            }
+        });
+
+
+        recyclerView.setAdapter(adapter);
+
         return view;
     }
 }
