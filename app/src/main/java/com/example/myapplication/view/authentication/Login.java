60112bc590bab62b7b1f930f10a9ab35585a964a
KhacSon0907 <creatine0907004@gmail.com>
2025-03-19 21:32:27 +0700

    update

--- app/src/main/java/com/example/myapplication/view/authentication/Login.java
+++ app/src/main/java/com/example/myapplication/view/authentication/Login.java
@@ -1,6 +1,9 @@
 package com.example.myapplication.view.authentication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +12,21 @@ import com.example.myapplication.R;
 
 public class Login extends AppCompatActivity {
 
+    private TextView tvSignUp;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        tvSignUp = findViewById(R.id.tvSignUp);
+
+        tvSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, Register.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/myapplication/view/authentication/Register.java
+++ app/src/main/java/com/example/myapplication/view/authentication/Register.java
@@ -1,5 +1,8 @@
 package com.example.myapplication.view.authentication;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Patterns;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
@@ -25,6 +28,20 @@ public class Register extends AppCompatActivity {
     private TextView tvSignIn;
     private AuthViewModel authViewModel;
 
+    private boolean isValidUsername(String username) {
+        String usernamePattern = "^[a-zA-Z\\s]+$";
+        return username.matches(usernamePattern) && username.length() > 4;
+    }
+
+    private boolean isValidPhoneNumber(String phoneNumber) {
+        // Kiểm tra số điện thoại có đúng 10 số và chỉ chứa chữ số
+        return phoneNumber.length() == 10 && phoneNumber.matches("\\d+");
+    }
+
+    private boolean isValidEmail(String email) {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,36 +63,39 @@ public class Register extends AppCompatActivity {
         });
 
         btnRegister.setOnClickListener(v -> {
-            // Ẩn bàn phím
             InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
             if (imm != null) {
                 imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
             }
 
-            // Lấy dữ liệu từ các trường nhập liệu
             String email = edtMail.getText().toString().trim();
-
             String username = edtFullName.getText().toString().trim();
             String password = edtPassword.getText().toString().trim();
             String phonenumber = edtPhoneNumber.getText().toString().trim();
             String confirmPassword = edtConfirmPassword.getText().toString().trim();
 
-            // Kiểm tra dữ liệu nhập
             if (email.isEmpty() || username.isEmpty() || password.isEmpty() || phonenumber.isEmpty()) {
                 showAlertDialog("Lỗi", "Vui lòng điền đầy đủ thông tin!");
                 return;
             }
-
-            // Kiểm tra mật khẩu và xác nhận mật khẩu
+            if (!isValidEmail(email)) {
+                showAlertDialog("Lỗi", "Vui lòng nhập đúng định dạng email!");
+                return;
+            }
+            if (!isValidPhoneNumber(phonenumber)) {
+                showAlertDialog("Lỗi", "Số điện thoại phải có đúng 10 chữ số!");
+                return;
+            }
             if (!password.equals(confirmPassword)) {
                 showAlertDialog("Lỗi", "Mật khẩu và xác nhận mật khẩu không khớp!");
                 return;
             }
+            if (!isValidUsername(username)) {
+                showAlertDialog("Lỗi", "Tên không được chứa ký tự đặc biệt và phải dài hơn 6 ký tự!");
+                return;
+            }
 
-            // Tạo đối tượng RegisterRequest
             RegisterRequest registerRequest = new RegisterRequest(email, username, password, phonenumber);
-
-            // Gọi phương thức đăng ký từ ViewModel
             authViewModel.registerUser(registerRequest);
         });
 
@@ -84,11 +104,22 @@ public class Register extends AppCompatActivity {
             public void onChanged(String result) {
                 // Hiển thị thông điệp đăng ký lên giao diện
                 Toast.makeText(Register.this, result, Toast.LENGTH_SHORT).show();
+
+                // Kiểm tra nếu đăng ký thành công thì chuyển sang trang Login sau 1 giây
+                if (result.equals("Đăng ký thành công!")) { // Thay bằng chuỗi thông báo từ ViewModel
+                    new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            Intent intent = new Intent(Register.this, Login.class);
+                            startActivity(intent);
+                            finish(); // Đóng activity hiện tại
+                        }
+                    }, 1000); // 1000 milliseconds = 1 giây
+                }
             }
         });
     }
 
-    // Hàm hiển thị AlertDialog
     private void showAlertDialog(String title, String message) {
         new AlertDialog.Builder(this)
                 .setTitle(title)
